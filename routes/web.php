d52f95a7428e6ee245a2386f068ae7d5e1a0444e
Sanubinha <sanuncieldamacenocbranco@gmail.com>
2024-10-24 15:52:21 -0300

    novas-rotas

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\CadastrosController;
 use App\Http\Controllers\DiretorController;
+use App\Http\Controllers\EquipamentosController;
 use App\Http\Controllers\PrefeituraController;
 use App\Http\Controllers\ProfessorController;
+use App\Http\Controllers\ReservaController;
 use App\Http\Controllers\TipoUserController;
 use App\Http\Controllers\UsrController;
 use Illuminate\Support\Facades\Route;
@@ -22,30 +24,45 @@ Route::get('/cadastro/professor', function() {return view('cadastro.professor');
 Route::post('/cadastro/professor', [ProfessorController::class, 'cadastrar']);
 
 Route::get('/cadastro/proReitura', function() {return view('cadastro.proReitura');})->name('cadastroProReitura');
+Route::post('/cadastro/proReitura', [UsrController::class, 'cadastrar']);
 
 Route::get('/cadastro/prefeitura', function() {return view('cadastro.prefeitura');})->name('cadastroPrefeitura');
-Route::post('/cadastro/prefeitura', [PrefeituraController::class, 'cadastrar']);
+Route::post('/cadastro/prefeitura', [UsrController::class, 'cadastrar']);
 
 Route::get('/cadastro/diretor', function() {return view('cadastro.diretor');})->name('cadastroDiretor');
-Route::post('/cadastro/diretor', [DiretorController::class, 'cadastrar']);
+Route::post('/cadastro/diretor', [UsrController::class, 'cadastrar']);
 
 Route::get('/login', function(){return view('login');})->name('login');
 Route::post('/login', [UsrController::class, 'logar']);
 Route::get('/logout', [UsrController::class, 'logout'])->name('logout');
 
+Route::get('/show/salas', [CadastrosController::class, 'index'])->name('ShowSalas');
+Route::get('/show/equipamentos', [CadastrosController::class, 'showEquip'])->name('ShowEquipamentos');
 
 Route::middleware(['auth', TipoUserController::class])->group(function () {
     Route::get('/usuario/{id_usuario}/edit', [UsrController::class, 'edit'])->name('editUser');
     Route::put('/usuarios/{id_usuario}', [UsrController::class, 'update'])->name('updtUser');
-    Route::get('/show/salas', [CadastrosController::class, 'index'])->name('ShowSalas');
-    Route::get('/show/equipamentos', [CadastrosController::class, 'showEquip'])->name('ShowEquipamentos');
-    
+
+
+    //Rotas específicas pro reitoria
+    Route::get('/reserva/pro_reitoria', [ReservaController::class, 'proReiReserva'])->name('ProReiReservaSalas');
+    Route::post('/reserva/pro_reitoria', [ReservaController::class, 'proReiStore']);
+
+    //Rotas específicas professor
+    Route::get('/reserva/professor', [ReservaController::class, 'profReserva'])->name('ProReservaSalas');
+    Route::post('/reserva/professor', [ReservaController::class, 'profStore']);
     
     //Rotas específicas prefeitura
     Route::get('/cadastro/salas', function () {return view('cadastro.salas');})->name('PrefCadastroSalas');
     Route::post('/cadastro/salas', [CadastrosController::class, 'cadastroSala']);
+    Route::get('/reserva/aguardo', [ReservaController::class, 'showReservas'])->name('PreReservaSalas');
 
     //Rotas específicas Diretor
     Route::get('/cadastro/equipamentos', function () {return view('cadastro.equipamentos');})->name('DirCadastroEquipamentos');
-    Route::post('/cadastro/equipamentos', [CadastrosController::class, 'cadastrarEquipamento']);
+    Route::post('/cadastro/equipamentos', [EquipamentosController::class, 'cadastrarEquipamento']);
+    Route::get('/reserva/diretor', function () {return view('reservas.diretor');})->name('DirReservaSalas');
 });
+
+Route::get('/equipamentos/{id}/edit', [EquipamentosController::class, 'edit'])->name('DirEditEquipamentos');
+Route::put('/equipamentos/{id}', [EquipamentosController::class, 'update'])->name('DirAtualizaEquipamentos');
+Route::delete('/equipamentos/{id}', [EquipamentosController::class, 'destroy'])->name('DirDestroyEquipamentos');
